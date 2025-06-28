3408423d28f773dc783c171282dca18d5e2e136e
Syed Musab <68772934+MusabBoltX@users.noreply.github.com>
2025-06-28 22:30:54 +0500

    Fixed VariantInfo

--- lib/src/hls/hls_parser/variant_info.dart
+++ lib/src/hls/hls_parser/variant_info.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart'; // for Object.hash
 import 'package:flutter/material.dart';
 
 class VariantInfo {
@@ -9,23 +10,10 @@ class VariantInfo {
     this.captionGroupId,
   });
 
-  /// The bitrate as declared by the EXT-X-STREAM-INF tag. */
   final int? bitrate;
-
-  /// The VIDEO value as defined in the EXT-X-STREAM-INF tag, or null if the VIDEO attribute is not
-  /// present.
   final String? videoGroupId;
-
-  /// The AUDIO value as defined in the EXT-X-STREAM-INF tag, or null if the AUDIO attribute is not
-  /// present.
   final String? audioGroupId;
-
-  /// The SUBTITLES value as defined in the EXT-X-STREAM-INF tag, or null if the SUBTITLES
-  /// attribute is not present.
   final String? subtitleGroupId;
-
-  /// The CLOSED-CAPTIONS value as defined in the EXT-X-STREAM-INF tag, or null if the
-  /// CLOSED-CAPTIONS attribute is not present.
   final String? captionGroupId;
 
   @override
@@ -41,6 +29,11 @@ class VariantInfo {
   }
 
   @override
-  int get hashCode => hashValues(
-      bitrate, videoGroupId, audioGroupId, subtitleGroupId, captionGroupId);
+  int get hashCode => Object.hash(
+        bitrate,
+        videoGroupId,
+        audioGroupId,
+        subtitleGroupId,
+        captionGroupId,
+      );
 }
